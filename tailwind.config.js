97929ea5d0d3f45c00b55f9c49e8f4c02fde476b
Richard <richarddiaznaranjo160@gmail.com>
2024-09-29 21:03:57 -0500

    A

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    colors: {
+      Neon: '#c3da32'
+    },
+  },
+  plugins: [],
+}
